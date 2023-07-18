bc2f142f544073c1095fc74250a37b9309c5fb3e
DanielMariscal <mariscaldaniel@gmail.com>
2023-07-17 23:39:50 -0400

    v6

--- client/src/components/Detail/Detail.jsx
+++ client/src/components/Detail/Detail.jsx
@@ -3,27 +3,31 @@ import { useParams } from "react-router-dom";
 import style from "./detail.module.css";
 import axios from "axios";
 import Pay from "../Pay/Pay";
+
 function Detail() {
   const { id } = useParams();
   const [comic, setComic] = React.useState({});
+
   async function fetcIdComics() {
     const data = await axios.get(`https://marvel-back-hc4s.onrender.com/comics/${id}`);
     return data.data;
   }
+
   React.useEffect(() => {
     async function fetch() {
       const data = await fetcIdComics();
       setComic(data);
     }
     fetch();
-  }, [fetcIdComics]);
+  }, []); // <-- Arreglo de dependencias vacío
+
   return (
     <div className={style.container}>
       <div className={style.biblio}>
         <h1>{comic.title}</h1>
         <h2>{comic.author}</h2>
         <p>{comic.description}</p>
-      <Pay price={comic.price}></Pay>
+        <Pay price={comic.price}></Pay>
       </div>
       <div>
         <img src={comic.img} alt="img" width="300px" />
